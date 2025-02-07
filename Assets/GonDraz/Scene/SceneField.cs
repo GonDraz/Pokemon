3850b9ff681ce4e4fad5808277943d0cc14c44a8
Vũ Ngọc Hải Hà <vungochaiha@gmail.com>
2025-02-07 17:30:43 +0700

    add Scene

---
+++ Assets/GonDraz/Scene/SceneField.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace GonDraz.Scene
+{
+    [Serializable]
+    public class SceneField
+    {
+        [SerializeField] private Object sceneAsset;
+
+        [SerializeField] private string sceneName = "";
+
+        public string SceneName => sceneName;
+
+        public static implicit operator string(SceneField sceneField)
+        {
+            return sceneField.SceneName;
+        }
+    }
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(SceneField))]
+    public class SceneFieldPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, GUIContent.none, property);
+            var sceneAsset = property.FindPropertyRelative("sceneAsset");
+            var sceneName = property.FindPropertyRelative("sceneName");
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            if (sceneAsset != null)
+            {
+                sceneAsset.objectReferenceValue = EditorGUI.ObjectField(position, sceneAsset.objectReferenceValue,
+                    typeof(SceneAsset), false);
+
+                if (sceneAsset.objectReferenceValue != null)
+                    sceneName.stringValue = (sceneAsset.objectReferenceValue as SceneAsset)?.name;
+            }
+
+            EditorGUI.EndProperty();
+        }
+    }
+#endif
+}
